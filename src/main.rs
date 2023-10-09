0262f4c161e48ee504f2275bf0953283ff646b9f
jh05013 <jh05013@naver.com>
2023-10-09 12:02:19 +0900

    파일 읽기 추가

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct 실행인자 {
+    프로그램_위치: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let 인자 = 실행인자::parse();
+    let 프로그램 =
+        std::fs::read_to_string(인자.프로그램_위치).expect("프로그램을 불러올 수 없습니다.");
+    println!("{프로그램:?}");
 }

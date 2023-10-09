a7eb5a044044375860d524fc9008c3c5635fadaf
jh05013 <jh05013@naver.com>
2023-10-09 13:26:49 +0900

    한글을 추가

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+// #[path = "격자/mod.rs"]
+// pub mod 격자;
+
+#[path = "한글.rs"]
+pub mod 한글;
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
-use clap::Parser;
+use clap::Parser as 해독기;
+use std::fmt::Debug as 오류해결;
+use String as 문자열;
 
-#[derive(Parser, Debug)]
+// use aheui::격자;
+
+#[derive(해독기, 오류해결)]
 #[command(author, version, about)]
 struct 실행인자 {
-    프로그램_위치: String,
+    프로그램_위치: 문자열,
 }
 
 fn main() {
     let 인자 = 실행인자::parse();
     let 프로그램 =
         std::fs::read_to_string(인자.프로그램_위치).expect("프로그램을 불러올 수 없습니다.");
-    println!("{프로그램:?}");
+    //println!("{:?}", 격자::격자로_읽기(프로그램));
 }
